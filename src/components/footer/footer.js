601a11c0ad2b2fb7c90e9dcf577a4c24f2d20116
Hema <khemalatha507@gmail.com>
2020-09-28 10:31:26 -0400

    created footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./footer.css";
+import githubIcon from "./github.png";
+import linkedinIcon from "./linkedin.PNG";
+
+function Footer() {
+    return (
+        <footer className="row pt-2 pb-4 foot justify-content-center">
+            <div className="mr-3 name">Hema</div>
+            <a href="https://github.com/hema0618">
+                <img className="linkicon mr-3" title="Github Profile" src={githubIcon}/>
+            </a>
+            <a href="https://www.linkedin.com/in/komatineni-hemalatha-628b2995/">
+                <img className="linkicon" title="Linkedin Profile" src={linkedinIcon}/>
+            </a>
+        </footer>
+    );
+  }
+
+export default Footer;
